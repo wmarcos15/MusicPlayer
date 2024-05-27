5bd4663abb4bb94427b21d45b712a62868b6635a
wmarcos15 <guillermoml9031@gmail.com>
2024-05-27 20:17:59 +0200

    Empty directory path for user configuration

--- config/config.h
+++ config/config.h
@@ -1,5 +1,5 @@
-#define PATH "/Users/guillermomarcoslara/Music/Library/" //default path in which the music directories will be located
-#define STARTING_DIR "Rap/"
+#define PATH "" //default path in which the music directories will be located
+#define STARTING_DIR "" //should not be the entire path, only the name of the dir inside of PATH
 #define WIDTH 1000
 #define HEIGHT 600
 #define FONT_SIZE 20
